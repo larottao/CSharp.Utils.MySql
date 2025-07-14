35abf85047ad4626bf3952fbc544d47bf746ed3c
larottao <larottao@gmail.com>
2025-07-13 21:15:29 -0600

    Method refactoring in progress

--- LaRottaO.CSharp.MySqlUtilities/Select.cs
+++ LaRottaO.CSharp.MySqlUtilities/Select.cs
@@ -10,7 +10,7 @@ namespace LaRottaO.CSharp.MySqlUtilities
 {
     public class Select
     {
-        public async Task<Tuple<Boolean, String, List<T>>> select<T>(string argConnString, String argQuery, int argTimeoutMs, Boolean showDebug = false) where T : new()
+        public async Task<(Boolean success, String errorReason, List<T> data)> select<T>(string argConnString, String argQuery, int argTimeoutMs, Boolean showDebug = false) where T : new()
         {
             if (showDebug)
             {
@@ -35,21 +35,22 @@ namespace LaRottaO.CSharp.MySqlUtilities
 
                 rdr = await cmd.ExecuteReaderAsync();
 
-                //  Create a dictionary that contains each column name and a consecutive number. That number will be later  used to locate the column by its name.
+                //  Create a dictionary that contains each column name and a consecutive number.
+                //  That number will be later  used to locate the column by its name.
 
                 Dictionary<String, int> dictionaryColumnNameVsIndex = new Dictionary<String, int>();
 
                 for (int i = 0; i < rdr.FieldCount; i++)
                 {
-                    String nombreColumna = rdr.GetName(i);
-                    dictionaryColumnNameVsIndex.Add(nombreColumna, i);
+                    String columnName = rdr.GetName(i);
+                    dictionaryColumnNameVsIndex.Add(columnName, i);
                 }
 
                 PropertyInfo[] properties = typeof(T).GetProperties();
 
                 T destinationObject;
 
-                List<T> listaSalida = new List<T>();
+                List<T> outputList = new List<T>();
 
                 while (rdr.Read())
                 {
@@ -93,30 +94,28 @@ namespace LaRottaO.CSharp.MySqlUtilities
                                     Console.WriteLine(errorMessageForConsole);
                                     Debug.WriteLine(errorMessageForConsole);
 
-                                    return new Tuple<Boolean, String, List<T>>(false, Constants.MYSQL_ERROR + errorMessageForConsole, new List<T>());
+                                    return (false, Constants.MYSQL_ERROR + errorMessageForConsole, new List<T>());
                                 }
                             }
                         }
                     }
 
-                    //  After all rows have been processed, return the object list
-
-                    listaSalida.Add(destinationObject);
+                    outputList.Add(destinationObject);
                 }
 
-                if (listaSalida.Count == 0)
+                if (outputList.Count == 0)
                 {
-                    return new Tuple<Boolean, String, List<T>>(false, Constants.MYSQL_NO_RESULTS, listaSalida);
+                    return (false, Constants.MYSQL_NO_RESULTS, outputList);
                 }
 
-                return new Tuple<Boolean, String, List<T>>(true, Constants.MYSQL_SUCCESS, listaSalida);
+                return (true, Constants.MYSQL_SUCCESS, outputList);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message + " " + ex.StackTrace);
                 Debug.WriteLine(ex.Message + " " + ex.StackTrace);
 
-                return new Tuple<Boolean, String, List<T>>(false, Constants.MYSQL_ERROR + " " + ex, new List<T>());
+                return (false, Constants.MYSQL_ERROR + " " + ex, new List<T>());
             }
             finally
             {
